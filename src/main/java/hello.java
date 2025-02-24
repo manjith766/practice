53d910bf71ef5e211c665f0f25bffa8cea4bc848
w3schools-test <test@w3schools.com>
2025-02-24 10:34:44 +0530

    student

---
+++ src/main/java/hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("student name = ram");
+        System.out.println("student age = 22");
+        System.out.println("student class = 5th");
+    }
+}
